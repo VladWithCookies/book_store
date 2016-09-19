9940a3c66748b9b92ac54b92f68defed7d9ccae4
Vlad V <summervibeswith9pm@gmail.com>
2016-09-19 18:28:36 +0300

    add form hidding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require jquery.barrating.min.js
-//= require_tree ../../../vendor/assets/javascripts/.
 //= require_tree .
 
 $(document).ready(function() {
